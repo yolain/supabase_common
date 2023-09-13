6112d241fa8674181b12b7cf2f2255707de9048a
yolain <yolain@163.com>
2023-09-13 19:23:41 +0800

    fix:import error

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 export * from "./headers.ts";
 export * as utils from "./utils.ts";
-export * from "./abstract.ts";
 export * from "./token.ts";
 export * from "./bcrypt.ts";
 export * as TOTP from "./otp.ts";
